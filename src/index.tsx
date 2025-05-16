b6911a81643d5d572b27a4d52e6fc1af94aa0f12
Utk Utk <utkarsh.kumar@oberoi-is.org>
2025-05-16 21:10:50 +0530

    bug fixed about user name and the react router wrapping

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'  // Tailwind
 import 'antd/dist/reset.css';
-
-
+import { BrowserRouter } from 'react-router-dom';
 
 const rootEl = document.getElementById('root')
 if (!rootEl) throw new Error('No root element found')
 
 ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )

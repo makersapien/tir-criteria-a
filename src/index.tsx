c643021cd187535525882093650b404774420008
Utk Utk <utkarsh.kumar@oberoi-is.org>
2025-06-06 19:31:31 +0530

    error resolving try 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'  // Tailwind
-import 'antd/dist/reset.css';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css'
 
 const rootEl = document.getElementById('root')
 if (!rootEl) throw new Error('No root element found')
 
 ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
